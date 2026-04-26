71eef0b0dd2458b96024f77f5cd63902f7789ba6
Delaney Kelley <kelleyde@mail.gvsu.edu>
2026-04-26 00:55:13 -0400

    Level Scene
    Added Levels Scene

--- Assets/Scenes/MainMenu.cs
+++ Assets/Scenes/MainMenu.cs
@@ -6,9 +6,28 @@ using System.Collections.Generic;
 // used this for tutorial: https://www.youtube.com/watch?v=zc8ac_qUXQY&t=2s
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    //public void PlayGame()
+    //{
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    //}
+    public void GoToLvl1()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("level1"); // replace "level1" with level name. Add scene to File -> Build Profiles -> Scene List
+    }
+
+    public void GoToLvl2()
+    {
+        SceneManager.LoadScene("level2"); // replace "level2" with level name. Add scene to File -> Build Profiles -> Scene List
+    }
+
+    public void GoToLvl3()
+    {
+        SceneManager.LoadScene("level3"); // replace "level3" with level name. Add scene to File -> Build Profiles -> Scene List
+    }
+
+    public void GoToLvls()
+    {
+        SceneManager.LoadScene("LevelsScene"); // replace "level3" with level name. Add scene to File -> Build Profiles -> Scene List
     }
 
     public void QuitGame()
